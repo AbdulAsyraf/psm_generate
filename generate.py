74ba6888c24e963113a81aac6a46f5ab256704fa
AbdulAsyraf <abdul.asyrafah@gmail.com>
2020-08-15 03:08:15 +0800

    As good as it's gonna be i guess

--- generate.py
+++ generate.py
@@ -5,13 +5,52 @@ startDate = datetime.datetime(2020, 1, 1)
 
 currDate = startDate
 
+#   bedroom, bathroom, living room, dining room, kitchen
+
+timetable = {
+    '00:00':[80, 40, 50, 20, 20],    #sleep
+    '01:00':[80, 40, 50, 20, 20],
+    '02:00':[80, 40, 50, 20, 20],
+    '03:00':[80, 40, 50, 20, 20],
+    '04:00':[80, 40, 50, 20, 20],
+    '05:00':[80, 40, 50, 20, 20],
+    '06:00':[40, 80, 60, 40, 40],    #wake up
+    '07:00':[20, 30, 70, 90, 80],    #breakfast
+    '08:00':[20, 30, 70, 90, 80],
+    '09:00':[30, 40, 80, 70, 50],    #lepak
+    '10:00':[30, 40, 80, 70, 50],
+    '11:00':[30, 40, 80, 70, 50],
+    '12:00':[30, 40, 80, 70, 50],
+    '13:00':[20, 30, 70, 90, 80],    #lunch
+    '14:00':[30, 40, 80, 70, 50],
+    '15:00':[30, 40, 80, 70, 50],
+    '16:00':[30, 40, 80, 70, 50],
+    '17:00':[40, 80, 60, 40, 40],
+    '18:00':[20, 30, 70, 90, 80],    #dinner
+    '19:00':[30, 40, 80, 70, 50],
+    '20:00':[30, 40, 80, 70, 50],
+    '21:00':[80, 40, 50, 20, 20],    #sleep
+    '22:00':[80, 40, 50, 20, 20],
+    '23:00':[80, 40, 50, 20, 20]
+}
+
 # currYear = currDate.year
 # currMonth = currDate.month
 # currDay = currDate.day
 
 while int(currDate.year) <= 2020:
-    filename = f"{currDate.year:04d}" + "-" + f"{currDate.month:02d}" + "-" f"{currDate.day:02d}" + ".csv"
-    print(filename)
+    filename = "data/" + f"{currDate.year:04d}" + "-" + f"{currDate.month:02d}" + "-" f"{currDate.day:02d}" + ".csv"
+    # print(filename)
     # print(f"{currDate.year:04d}" + "-" + f"{currDate.month:02d}" + "-" + f"{currDate.day:02d}" + "  " + f"{currDate.hour:02d}" + ":" + f"{currDate.minute:02d}")
+    stamp = f"{currDate.hour:02d}" + ":" + f"{currDate.minute:02d}"
+    
+    with open(filename, 'a', newline='') as csvfile:
+        csvw = csv.writer(csvfile)
+        row = [stamp]
+        for i in range(5):
+            row.append(timetable[stamp][i])
+
+        csvw.writerow(row)
+    
     currDate = currDate + datetime.timedelta(hours=1)
     
